5fd82a70a26f49e75da02dc52cbb194d6d1638d1
EdgarSnow75 <119287727+EdgarSnow75@users.noreply.github.com>
2024-09-15 02:29:36 +0700

    Add condition checks and loops for reverse string
    String argument is first split and checked if there are more than one
    element in the returned array. This way, the string can be revered solo
    or if there are more than one element in the array, split each element and
    reverse the arrays. Used join to change arrays back into strings.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,27 @@
-const reverseString = function() {
-
+/* Create a variable to store the final result
+   First check if the string includes space or comma
+   Assign an array to the split string using space or comma as separators
+   Reverse the elements of the array
+   Run a callback function to reverse each characters of the element by looping through them
+   Save the final array as string to the result variable  */
+const reverseString = function (string) {
+  let changedString = "";
+  const splitArray = string.split(" ");
+  if (splitArray.length === 1) {
+    changedString = string.split("");
+    changedString.reverse();
+    changedString = changedString.join("");
+    console.log(changedString);
+  } else if (splitArray.length > 1) {
+    splitArray.reverse();
+    for (let i = 0; i < splitArray.length; i++) {
+      const itemArray = splitArray[i].split("");
+      itemArray.reverse();
+      splitArray[i] = itemArray.join("");
+    }
+    changedString = splitArray.join(" ");
+  }
+  return changedString;
 };
 
 // Do not edit below this line
