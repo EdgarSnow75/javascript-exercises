258bb1949deabaaa0a341280472df2abe20a1696
EdgarSnow75 <119287727+EdgarSnow75@users.noreply.github.com>
2024-09-15 18:39:07 +0700

    Add branching logic for finding leap years
    Two branching and one return statement as a semi-branching were needed
    to deal with all three requirements to find a leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      return year % 400 == 0;
+    } else {
+      return true;
+    }
+  }
+  return false;
 };
 
+leapYears(1997);
 // Do not edit below this line
 module.exports = leapYears;
